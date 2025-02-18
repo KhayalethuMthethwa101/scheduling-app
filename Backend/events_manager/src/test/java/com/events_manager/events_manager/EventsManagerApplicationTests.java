85cb196999e7babb850870f78f2cabe91e2a3cb0
KhayalethuMthethwa101 <khayalethumthethwa16@gmail.com>
2025-02-18 21:45:38 +0200

    docker info

--- Backend/events_manager/src/test/java/com/events_manager/events_manager/EventsManagerApplicationTests.java
+++ Backend/events_manager/src/test/java/com/events_manager/events_manager/EventsManagerApplicationTests.java
@@ -1,13 +1,13 @@
 package com.events_manager.events_manager;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EventsManagerApplicationTests {
+// @SpringBootTest
+// class EventsManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
